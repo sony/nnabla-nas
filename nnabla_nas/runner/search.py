4f56190594f8bddcdc71950cf0608d3e65dee4cc
Bac Nguyen Cong <denguyeb@destc0strapp78.eu.sony.com>
2020-02-06 18:44:51 +0100

    update

--- nnabla_nas/runner/search.py
+++ nnabla_nas/runner/search.py
@@ -42,7 +42,7 @@ class Searcher(object):
         self.one_epoch = len(self.dataloader['train']) // args.bs_train
         self.monitor = ProgressMeter(self.one_epoch, path=args.output_path)
 
-        self.callbacks_at_beginning()
+        self.callback_on_start()
 
     def run(self):
         """Run the training process."""
@@ -52,14 +52,13 @@ class Searcher(object):
             lr = self.optimizer['train'].get_learning_rate()
             logger.info(f'Running epoch={cur_epoch}\tlr={lr:.5f}')
             for i in range(self.one_epoch):
-                self.update_model_step()
-                self.update_arch_step()
+                self.callback_model_on_update()
+                self.callback_arch_on_update()
                 if i % (self.args.print_frequency) == 0:
                     self.monitor.display(i)
-            # saving the architecture parameters
-            self.save_output()
-            # logging output
+            self.callback_on_update()
             self.monitor.write(cur_epoch)
+        self.callback_on_finish()
         self.monitor.close()
         return self
 
@@ -70,7 +69,7 @@ class Searcher(object):
             lr = self.optimizer['warmup'].get_learning_rate()
             logger.info(f'warm-up epoch={cur_epoch}\tlr={lr:.5f}')
             for i in range(self.one_epoch):
-                self.update_model_step(key='warmup')
+                self.callback_model_on_update(key='warmup')
                 if i % (self.args.print_frequency) == 0:
                     self.monitor.display(i)
 
@@ -111,61 +110,67 @@ class Searcher(object):
             self.model.get_arch_parameters(grad_only=True)
         )
 
-    def sample_network(self):
-        pass
-
-    def callbacks_at_beginning(self):
-        pass
-
-    def save_output(self):
+    def callback_on_update(self):
+        # save the model parameters
         nn.save_parameters(
             os.path.join(self.args.output_path, 'arch.h5'),
             self.model.get_parameters()
         )
 
-    def update_model_step(self):
+    def sample_network(self):
+        pass
+
+    def callback_on_finish(self):
+        pass
+
+    def callback_on_start(self):
+        pass
+
+    def callback_model_on_update(self):
         raise NotImplementedError
 
-    def update_arch_step(self):
+    def callback_arch_on_update(self):
         raise NotImplementedError
 
 
 class DartsSeacher(Searcher):
 
-    def callbacks_at_beginning(self):
+    def callback_on_start(self):
         self._sample_train_net()
         self._sample_search_net()
 
-    def update_model_step(self, key='train'):
+    def callback_model_on_update(self, key='train'):
         bz, p = self.args.mbs_train, self.placeholder['train']
+        input, target, loss, err = p['input'], p['target'], p['loss'], p['err']
         self.optimizer[key].zero_grad()
         for _ in range(self.accum_train):
-            p['input'].d, p['target'].d = self.dataloader['train'].next()
-            p['loss'].forward(clear_no_need_grad=True)
-            p['loss'].backward(clear_buffer=True)
-            p['err'].forward(clear_buffer=True)
-            loss, err = p['loss'].d.copy(),  p['err'].d.copy()
-            self.monitor.update('train_loss', loss * self.accum_train, bz)
-            self.monitor.update('train_err', err, bz)
+            input.d, target.d = self.dataloader['train'].next()
+            loss.forward(clear_no_need_grad=True)
+            loss.backward(clear_buffer=True)
+            err.forward(clear_buffer=True)
+            l, e = loss.d.copy(),  err.d.copy()
+            self.monitor.update('train_loss', l * self.accum_train, bz)
+            self.monitor.update('train_err', e, bz)
         self.optimizer[key].update()
 
-    def update_arch_step(self):
-        bz, p = self.args.mbs_valid, self.placeholder['valid']
+    def callback_arch_on_update(self):
         self._sample_search_net()
+        bz, p = self.args.mbs_valid, self.placeholder['valid']
+        input, target, loss, err = p['input'], p['target'], p['loss'], p['err']
         self.optimizer['valid'].zero_grad()
         for i in range(self.accum_valid):
-            p['input'].d, p['target'].d = self.dataloader['valid'].next()
-            p['loss'].forward(clear_buffer=True)
-            p['err'].forward(clear_buffer=True)
-            loss, err = p['loss'].d.copy(),  p['err'].d.copy()
-            self.monitor.update('valid_loss', loss * self.accum_valid, bz)
-            self.monitor.update('valid_err', err, bz)
+            input.d, target.d = self.dataloader['valid'].next()
+            loss.forward(clear_buffer=True)
+            err.forward(clear_buffer=True)
+            l, e = loss.d.copy(),  err.d.copy()
+            self.monitor.update('valid_loss', l * self.accum_valid, bz)
+            self.monitor.update('valid_err', e, bz)
         self.optimizer['valid'].update()
 
 
 class ProxylessNasSearcher(Searcher):
 
-    def callbacks_at_beginning(self):
+    def callback_on_start(self):
         self.arch_modules = self.model.get_arch_modules()
         self._reward = 0
 
@@ -173,22 +178,23 @@ class ProxylessNasSearcher(Searcher):
         for m in self.arch_modules:
             m.update_active_index()
 
-    def update_model_step(self, key='train'):
+    def callback_model_on_update(self, key='train'):
         """Update the model parameters."""
-        bz, p = self.args.mbs_train, self.placeholder['train']
         self._sample_train_net()
+        bz, p = self.args.mbs_train, self.placeholder['train']
+        input, target, loss, err = p['input'], p['target'], p['loss'], p['err']
         self.optimizer[key].zero_grad()
         for _ in range(self.accum_train):
-            p['input'].d, p['target'].d = self.dataloader['train'].next()
-            p['loss'].forward(clear_no_need_grad=True)
-            p['loss'].backward(clear_buffer=True)
-            p['err'].forward(clear_buffer=True)
-            loss, err = p['loss'].d.copy(),  p['err'].d.copy()
-            self.monitor.update('train_loss', loss * self.accum_train, bz)
-            self.monitor.update('train_err', err, bz)
+            input.d, target.d = self.dataloader['train'].next()
+            loss.forward(clear_no_need_grad=True)
+            loss.backward(clear_buffer=True)
+            err.forward(clear_buffer=True)
+            l, e = loss.d.copy(),  err.d.copy()
+            self.monitor.update('train_loss', l * self.accum_train, bz)
+            self.monitor.update('train_err', e, bz)
         self.optimizer[key].update()
 
-    def update_arch_step(self):
+    def callback_arch_on_update(self):
         """Update the arch parameters."""
         beta, n_iter = 0.9, 5
         bz, p = self.args.mbs_valid, self.placeholder['valid']
@@ -198,14 +204,16 @@ class ProxylessNasSearcher(Searcher):
         for _ in range(n_iter):
             reward = 0
             self._sample_search_net()
+            input, target, loss, err = (p['input'], p['target'],
+                                        p['loss'],  p['err'])
             for i in range(self.accum_valid):
-                p['input'].d, p['target'].d = data[i]
-                p['loss'].forward(clear_buffer=True)
-                p['err'].forward(clear_buffer=True)
-                loss, err = p['loss'].d.copy(),  p['err'].d.copy()
-                reward += (1 - err) / self.accum_valid
-                self.monitor.update('valid_loss', loss * self.accum_valid, bz)
-                self.monitor.update('valid_err', err, bz)
+                input.d, target.d = data[i]
+                loss.forward(clear_buffer=True)
+                err.forward(clear_buffer=True)
+                l, e = loss.d.copy(),  err.d.copy()
+                reward += (1 - e) / self.accum_valid
+                self.monitor.update('valid_loss', l * self.accum_valid, bz)
+                self.monitor.update('valid_err', e, bz)
             # adding contraints
             for k, v in self.regularizer.items():
                 value = v['reg'].get_estimation(self.model)
