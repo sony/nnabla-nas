06684b6b1b17b810199cbd2324bc096c9146873b
Bac Nguyen Cong <denguyeb@destc0strapp78.eu.sony.com>
2020-02-07 17:07:40 +0100

    update

--- jobs.sh
+++ jobs.sh
@@ -1,17 +1,5 @@
 # search DARTS
 python main.py -d 2 -f examples/darts_search.json --search -a DartsSearcher -o log/darts/search
 
-# validate DARTS
-python main.py -d 1 -f examples/darts_validate.json
-
 # search PNAS
 python main.py -d 1 -f examples/pnas_search.json
-
-# validate PNAS
-python main.py -d 2 -f examples/pnas_validate.json
-
-# search PNAS with constraints
-python main.py -d 2 -f examples/constraint_pnas_search.json
-
-# validate PNAS with constraints
-python main.py -d 2 -f examples/constraint_pnas_validate.json
