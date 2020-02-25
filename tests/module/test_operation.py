63795cc296dc30e85a4d6299598d78369fe45016
Bac Nguyen Cong <bac.nguyencong@sony.com>
2020-02-25 13:37:24 +0100

    add F.div2 and F.mul2

--- tests/module/test_operation.py
+++ tests/module/test_operation.py
@@ -5,7 +5,7 @@ from nnabla_nas.module import Lambda
 import pytest
 
 
-@pytest.mark.parametrize('func', [F.add2, F.sub2])
+@pytest.mark.parametrize('func', [F.add2, F.sub2, F.mul2, F.div2])
 def test_Lambda(func):
     module = Lambda(func)
     input1 = nn.Variable((8, 3, 32, 32))
@@ -17,8 +17,6 @@ def test_Lambda(func):
 
     input1.d = np.random.randn(*input1.shape)
     input2.d = np.random.randn(*input2.shape)
-
-    print(module)
 
     output.forward()
     assert not np.isnan(output.d).any()
