f2af0acc02a01dc1f9e82984d394a0484cb14902
Bac Nguyen Cong <bac.nguyencong@sony.com>
2020-03-09 16:59:09 +0100

    first setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,5 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+html_logo = "logo/logo.png"
---
+++ docs/source/introdution.rst
@@ -0,0 +1,11 @@
+============
+Introduction
+============
+
+.. toctree::
+    :maxdepth: 2
+
+
+What is neural network architecture search?
+-------------------------------------------
+
