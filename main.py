f0ffa8b30ce392167b1c4ec8b61bca44ea8e823f
Yukio Oobuchi <Yukio.Oobuchi@sony.com>
2020-05-27 11:38:45 +0900

    Build wheel and add license information to all files.

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
+# Copyright (c) 2020 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import json
 import os
+import sys
 
 import nnabla as nn
 import nnabla.functions as F
 from nnabla.ext_utils import get_extension_context
 from nnabla.logger import logger
-from nnabla_ext.cuda import StreamEventHandler
+
+try:
+    from nnabla_ext.cuda import StreamEventHandler
+except ModuleNotFoundError:
+    print("ERROR: You need to install NNabla CUDA extension by yourself.")
+    print("Select one of following that suits your environment.")
+    print("")
+    print("   pip install nnabla-ext-cuda90")
+    print("   pip install nnabla-ext-cuda100")
+    print("   pip install nnabla-ext-cuda101")
+    print("   pip install nnabla-ext-cuda102 (Comming soon)")
+    sys.exit(-1)
+except ImportError:
+    print("ERROR: CUDA extension installed but could not initialized.")
+    print(" Please make sure that your installed nnabla-ext-cuda??? appropriate for your environment.")
+    raise
+
 
 import nnabla_nas.contrib as contrib
 from args import Configuration
 from nnabla_nas import runner
 from nnabla_nas.utils.helper import CommunicatorWrapper, label_smoothing_loss
 
-if __name__ == "__main__":
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--context', '-c', type=str, default='cudnn',
                         help="Extension module. 'cudnn' is highly recommended.")
@@ -81,3 +113,7 @@ if __name__ == "__main__":
         evaluate=lambda o, t: F.mean(F.top_n_error(o, t)),
         args=conf
     ).run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2020 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    root_dir = os.path.realpath(os.path.dirname(__file__))
+    setup(
+        entry_points={"console_scripts": ["nnabla_nas=nnabla_nas.main:main"]},
+        install_requires=['graphviz',
+                          'h5py',
+                          'mako',
+                          'mypy',
+                          'networkx',
+                          'nnabla',
+                          'sklearn',
+                          'tensorboard',
+                          'tqdm'
+                          ],
+        package_dir={'nnabla_nas': 'nnabla_nas'},
+        packages=['nnabla_nas',
+                  'nnabla_nas.contrib',
+                  'nnabla_nas.contrib.darts',
+                  'nnabla_nas.contrib.mobilenet',
+                  'nnabla_nas.contrib.pnas',
+                  'nnabla_nas.contrib.random_wired',
+                  'nnabla_nas.contrib.zoph',
+                  'nnabla_nas.dataset',
+                  'nnabla_nas.dataset.cifar10',
+                  'nnabla_nas.dataset.imagenet',
+                  'nnabla_nas.module',
+                  'nnabla_nas.module.static',
+                  'nnabla_nas.optimizer',
+                  'nnabla_nas.runner',
+                  'nnabla_nas.runner.searcher',
+                  'nnabla_nas.runner.trainer',
+                  'nnabla_nas.utils',
+                  'nnabla_nas.utils.estimator',
+                  'nnabla_nas.utils.profiler',
+                  'nnabla_nas.utils.profiler.nnabla',
+                  'nnabla_nas.utils.profiler.snpe',
+                  'nnabla_nas.utils.tensorboard'],
+        name='nnabla_nas',
+        description='Use NNC compute resource from NNabla',
+        version='0.0.1',
+        classifiers=[
+                     'Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: Science/Research',
+                     'Topic :: Scientific/Engineering',
+                     'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                     'License :: OSI Approved :: Apache Software License',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Operating System :: POSIX :: Linux'
+                     ],
+        keywords="deep learning artificial intelligence machine learning neural network",
+        python_requires='>=3.6'
+        )
