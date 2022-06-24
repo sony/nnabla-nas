9f674ebb6d7a5523d60d683800159835b62dc8e2
richeekSony <Richeek.Das@sony.com>
2022-06-25 05:35:36 +0900

    Removed FusedBatchNorm and DwConv

--- nnabla_nas/contrib/common/ofa/layers.py
+++ nnabla_nas/contrib/common/ofa/layers.py
@@ -13,9 +13,8 @@
 # limitations under the License.
 
 from collections import OrderedDict
-import nnabla as nn
+
 import nnabla.functions as F
-from nnabla.initializer import ConstantInitializer
 
 from .... import module as Mo
 from .utils.common_tools import get_same_padding, min_divisible_value, make_divisible
@@ -78,6 +77,19 @@ def force_tuple2(value):
         assert len(value) == 2
         return value
     return (value,) * 2
+
+
+def build_activation(act_func, inplace=False):
+    if act_func == 'relu':
+        return Mo.ReLU(inplace=inplace)
+    elif act_func == 'relu6':
+        return Mo.ReLU6()
+    elif act_func == 'h_swish':
+        return Mo.Hswish()
+    elif act_func is None or act_func == 'none':
+        return None
+    else:
+        raise ValueError('do not support: %s' % act_func)
 
 
 class ResidualBlock(Mo.Module):
@@ -335,78 +347,6 @@ class SEModule(Mo.Module):
         return input * y
 
 
-def build_activation(act_func, inplace=False):
-    if act_func == 'relu':
-        return Mo.ReLU(inplace=inplace)
-    elif act_func == 'relu6':
-        return Mo.ReLU6()
-    elif act_func == 'h_swish':
-        return Mo.Hswish()
-    elif act_func is None or act_func == 'none':
-        return None
-    else:
-        raise ValueError('do not support: %s' % act_func)
-
-
-class DwConvLayer(Mo.Sequential):
-
-    r""" Depthwise Convolution-BatchNormalization(optional)-Activation layer.
-
-    Args:
-        in_channels (int): Number of convolution kernels (which is
-            equal to the number of input channels).
-        kernel (tuple of int, optional): Convolution kernel size. For
-            example, to apply convolution on an image with a 3 (height) by 5
-            (width) two-dimensional kernel, specify (3, 5). Defaults to (3, 3)
-        stride (tuple of int, optional): Stride sizes for
-            dimensions. Defaults to (1, 1).
-        dilation (tuple of int, optional): Dilation sizes for
-            dimensions. Defaults to (1, 1).
-        group (int, optional): Number of groups of channels.
-            Defaults to 1.
-        with_bias (bool, optional): If True, bias for Convolution is added.
-            Defaults to False.
-        use_bn (bool, optional): If True, BatchNormalization layer is added.
-            Defaults to True.
-        act_func (str, optional) Type of activation. Defaults to 'relu'.
-    """
-
-    def __init__(self, in_channels, kernel=(3, 3),
-                 stride=(1, 1), dilation=(1, 1), with_bias=False,
-                 use_bn=True, act_func='relu'):
-        self._in_channels = in_channels
-        self._kernel = kernel
-        self._stride = stride
-        self._dilation = dilation
-        self._with_bias = with_bias
-        self._use_bn = use_bn
-        self._act_func = act_func
-
-        padding = get_same_padding(self._kernel)
-        if isinstance(padding, int):
-            padding *= self._dilation
-        else:
-            new_padding = (padding[0] * self._dilation[0],
-                           padding[1] * self._dilation[1])
-            padding = tuple(new_padding)
-
-        module_dict = OrderedDict()
-        module_dict['conv'] = Mo.DwConv(self._in_channels, self._kernel,
-                                        pad=padding, stride=self._stride, dilation=self._dilation,
-                                        with_bias=self._with_bias)
-        if self._use_bn:
-            module_dict['bn'] = Mo.BatchNormalization(self._in_channels, 4)
-        module_dict['act'] = build_activation(act_func)
-
-        super(DwConvLayer, self).__init__(module_dict)
-
-    def build_from_config(config):
-        return DwConvLayer(**config)
-
-    def extra_repr(self):
-        return get_extra_repr(self)
-
-
 class SeparableConv(Mo.Module):
     def __init__(
             self, in_channels, out_channels, kernel=(1, 1),
@@ -516,62 +456,3 @@ class XceptionBlock(Mo.Module):
 
     def extra_repr(self):
         return get_extra_repr(self)
-
-
-class FusedBatchNormalization(Mo.Module):
-    def __init__(self, n_features, n_dims, z=None, axes=[1], decay_rate=0.9, eps=1e-5,
-                 nonlinearity='relu', output_stat=False, fix_parameters=False, param_init=None,
-                 name=''):
-        Mo.Module.__init__(self, name=name)
-        self._scope_name = f'<fusedbatchnorm at {hex(id(self))}>'
-
-        assert len(axes) == 1
-
-        shape_stat = [1 for _ in range(n_dims)]
-        shape_stat[axes[0]] = n_features
-
-        if param_init is None:
-            param_init = {}
-        beta_init = param_init.get('beta', ConstantInitializer(0))
-        gamma_init = param_init.get('gamma', ConstantInitializer(1))
-        mean_init = param_init.get('mean', ConstantInitializer(0))
-        var_init = param_init.get('var', ConstantInitializer(1))
-
-        if fix_parameters:
-            self._beta = nn.Variable.from_numpy_array(
-                beta_init(shape_stat))
-            self._gamma = nn.Variable.from_numpy_array(
-                gamma_init(shape_stat))
-        else:
-            self._beta = Mo.Parameter(shape_stat, initializer=beta_init,
-                                      scope=self._scope_name)
-            self._gamma = Mo.Parameter(shape_stat, initializer=gamma_init,
-                                       scope=self._scope_name)
-
-        self._mean = Mo.Parameter(shape_stat, need_grad=False,
-                                  initializer=mean_init,
-                                  scope=self._scope_name)
-        self._var = Mo.Parameter(shape_stat, need_grad=False,
-                                 initializer=var_init,
-                                 scope=self._scope_name)
-        self._z = z
-        self._axes = axes
-        self._decay_rate = decay_rate
-        self._eps = eps
-        self._n_features = n_features
-        self._fix_parameters = fix_parameters
-        self._output_stat = output_stat
-        self._nonlinearity = nonlinearity
-
-    def call(self, input):
-        return F.fused_batch_normalization(input, self._beta, self._gamma,
-                                           self._mean, self._var, self._z, self._axes,
-                                           self._decay_rate, self._eps,
-                                           self.training, self._nonlinearity, self._output_stat)
-
-    def extra_repr(self):
-        return get_extra_repr(self)
-
-    @staticmethod
-    def build_from_config(config):
-        return FusedBatchNormalization(**config)
