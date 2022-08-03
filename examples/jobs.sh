e99e111afe1248f07ce226f59178cc9aa59973b4
Lucas Yakhontoff <deyakhol@destc0strapp15>
2022-08-03 14:45:45 +0200

    fixed jobs.sh

--- examples/jobs.sh
+++ examples/jobs.sh
@@ -1,24 +1,24 @@
 #---------------------------- DARTS --------------------------------#
 # search DARTS
-python main.py experiment=darts/cifar10_search
+python main.py experiment=classification/darts/cifar10_search
 
 # train DARTS
-python main.py experiment=darts/cifar10_train
+python main.py experiment=classification/darts/cifar10_train
 #------------------------------------------------------------------#
 
 
 #---------------------------- PNAS --------------------------------#
 # search PNAS
-python main.py experiment=pnas/cifar10_search
+python main.py experiment=classification/pnas/cifar10_search
 
 # train PNAS
-python main.py experiment=pnas/cifar10_train
+python main.py experiment=classification/pnas/cifar10_train
 
 # search PNAS with latency constraints
-python main.py experiment=pnas/cifar10_search_latency
+python main.py experiment=classification/pnas/cifar10_search_latency
 
 # train PNAS with latency constraints
-python main.py experiment=pnas/cifar10_train_latency             
+python main.py experiment=classification/pnas/cifar10_train_latency             
 #------------------------------------------------------------------#
 
 
@@ -27,113 +27,113 @@ python main.py experiment=pnas/cifar10_train_latency
 # on cifar10 data set
 
 # search MobileNet
-python main.py experiment=mobilenet/cifar10_search
+python main.py experiment=classification/mobilenet/cifar10_search
 
 # train MobileNet
-python main.py experiment=mobilenet/cifar10_train
+python main.py experiment=classification/mobilenet/cifar10_train
 
 # search MobileNet with latency
-python main.py experiment=mobilenet/cifar10_search_latency
+python main.py experiment=classification/mobilenet/cifar10_search_latency
 # train MobileNet with latancy
-python main.py experiment=mobilenet/cifar10_train_latency
+python main.py experiment=classification/mobilenet/cifar10_train_latency
 
 # reference MobileNet V2
-python main.py experiment=mobilenet/cifar10_reference
+python main.py experiment=classification/mobilenet/cifar10_reference
 #------------------------------------------------------------------#
 
 
 
 #----------------------------Zoph---------------------------------#
 # search zoph search space with pnas and without latency constraint
-python main.py experiment=zoph/pnas_zoph_search
+python main.py experiment=classification/zoph/pnas_zoph_search
 
 # train zoph network
-python main.py experiment=zoph/zoph_train
+python main.py experiment=classification/zoph/zoph_train
 #------------------------------------------------------------------#
 
 
 # train random zoph network
-python main.py experiment=zoph/zoph_train_random
+python main.py experiment=classification/zoph/zoph_train_random
 
 # train random zoph network, using additions for merging
-python main.py experiment=zoph/zoph_train_random_merge_add
+python main.py experiment=classification/zoph/zoph_train_random_merge_add
 
 # continue training from checkpoint
-python main.py experiment=zoph/zoph_train_continue
+python main.py experiment=classification/zoph/zoph_train_continue
 #------------------------------------------------------------------#
 
 
 
 #---------------------------- RandomlyWired------------------------#
 # train randomly wired network
-python main.py experiment=random_wired/cifar10_train
+python main.py experiment=classification/random_wired/cifar10_train
 #------------------------------------------------------------------#
 
 
 
 #---------------------- ImageNet (PNAS)----------------------------#
 # search MobileNet network
-mpirun -n 4 python main.py experiment=mobilenet/imagenet_search
+mpirun -n 4 python main.py experiment=classification/mobilenet/imagenet_search
 
 # train MobileNet network
-mpirun -n 4 python main.py experiment=mobilenet/imagenet_train
+mpirun -n 4 python main.py experiment=classification/mobilenet/imagenet_train
 
 # search MobileNet network with latency
-mpirun -n 4 python main.py experiment=mobilenet/imagenet_search_latency
+mpirun -n 4 python main.py experiment=classification/mobilenet/imagenet_search_latency
 
 # train MobileNet network with latency
-mpirun -n 4 python main.py experiment=mobilenet/imagenet_train_latency
+mpirun -n 4 python main.py experiment=classification/mobilenet/imagenet_train_latency
 
 # ref MobileNet network
-mpirun -n 4 python main.py experiment=mobilenet/imagenet_reference
+mpirun -n 4 python main.py experiment=classification/mobilenet/imagenet_reference
 #------------------------------------------------------------------#
 
 
 
 #---------------------- FAIR NAS ----------------------------#
-mpirun -n 4 python main.py experiment=fairnas/cifar10_search
+mpirun -n 4 python main.py experiment=classification/fairnas/cifar10_search
 
-mpirun -n 4 python main.py experiment=fairnas/cifar10_train
+mpirun -n 4 python main.py experiment=classification/fairnas/cifar10_train
 
-mpirun -n 4 python main.py experiment=fairnas/imagenet_search
+mpirun -n 4 python main.py experiment=classification/fairnas/imagenet_search
 
-mpirun -n 4 python main.py experiment=fairnas/imagenet_train
+mpirun -n 4 python main.py experiment=classification/fairnas/imagenet_train
 
 #---------------------- OFA-MobileNetV3 (ImageNet) ----------------------------#
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_fullnet
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_fullnet
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_kernel
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_kernel
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_depth_phase1
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_depth_phase1
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_depth_phase2
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_depth_phase2
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_expand_phase1
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_expand_phase1
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_expand_phase2
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_expand_phase2
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_train_subnet
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_train_subnet
 #------------------------------------------------------------------#
 
 
 #---------------------- OFA-XCEPTION(ImageNet) ----------------------------#
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_fullnet
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_fullnet
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_kernel
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_kernel
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_depth_phase1
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_depth_phase1
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_depth_phase2
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_depth_phase2
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_expand_phase1
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_expand_phase1
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_search_expand_phase2
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_search_expand_phase2
 
-mpirun -n 4 python main.py experiment=ofa/ofa_xception/imagenet_train_subnet
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_xception/imagenet_train_subnet
 #------------------------------------------------------------------#
 
 
 #---------------------- CompOFA-MobileNetV3 (ImageNet) ----------------------------#
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_comp_phase1
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_comp_phase1
 
-mpirun -n 4 python main.py experiment=ofa/ofa_mbv3/imagenet_search_comp_phase2
+mpirun -n 4 python main.py experiment=classification/ofa/ofa_mbv3/imagenet_search_comp_phase2
